2c6d8a58226778745a7b7a33f054306f2ec7c288
akephisit <kruakemaths@gmail.com>
2025-08-04 17:18:54 +0700

    เพิ่ม shadcnui-svelte

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		alias: {
+			"@/*": "./path/to/lib/*",
+		},
+		adapter: adapter()
+	}
 };
 
 export default config;
